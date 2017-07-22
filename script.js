88880236e9ee6e24af1a86442334d9e83bbb987e
Joel Schulz-Andres <jschulz-andres@localhost.localdomain>
2017-07-22 08:42:53 +0200

    fixed new bug with wrong expansion of table

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ function expandFields(index, diff) {
 
 	for (var i = index; i < index+diff; i++) {
 		// Create an empty <tr> element and add it to the 1st position of the table:
-		var row = table.insertRow(numberOfInstructionFields+1)
+		var row = table.insertRow(numberOfInstructionFields)
 		// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
 		var cell1 = row.insertCell(0)
 		var cell2 = row.insertCell(1)
